385251b587e9fc2b6ac1774d1c372776d54d13e3
amitihere <amiti.aneesh1@gmail.com>
2026-04-12 18:16:50 +0530

    feat: register expense routes in the system

---
+++ backend/src/routes/expenseRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { ExpenseController } from "../controllers/ExpenseController";
+
+export class ExpenseRoutes {
+  public router: Router;
+
+  constructor(private controller: ExpenseController) {
+    this.router = Router();
+    this.setupRoutes();
+  }
+
+  private setupRoutes(): void {
+    // employee routes
+    this.router.post("/", this.controller.create);
+    this.router.get("/mine", this.controller.getMine);
+
+    // action routes
+    this.router.post("/:id/submit", this.controller.submit);
+    this.router.post("/:id/approve", this.controller.approve);
+    this.router.post("/:id/reject", this.controller.reject);
+    this.router.post("/:id/pay", this.controller.markPaid);
+
+    // query routes
+    this.router.get("/", this.controller.getAll);
+    this.router.get("/:id/logs", this.controller.getLogs);
+  }
+}
